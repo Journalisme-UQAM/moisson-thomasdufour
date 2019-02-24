2f9ede8ce3dd836546f1218c890a07899f571eed
thomasdufour <33037172+thomasdufour@users.noreply.github.com>
2019-02-24 14:37:22 -0500

    Update moisson.py

--- moisson.py
+++ moisson.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-#Ce code permet d'aller chercher tous les articles du Devoir entre le 1er janvier 2012 et le 24 février 2018
+#Ce code permet d'aller chercher tous les articles du Devoir entre le 1er janvier 2012 et le 24 février 2019
 #Il permet aussi de compter l'occurence de chaque pays du monde dans les différents articles
 
 import csv
